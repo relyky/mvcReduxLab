5a754b83b5347f98ad7ad82f2e9e7a54a493d99b
Rely Ky <relyky@hotmail.com>
2019-04-16 18:06:43 +0800

    step

--- mvcReduxLab/Areas/MVCLab/Controllers/ReportDemoController.cs
+++ mvcReduxLab/Areas/MVCLab/Controllers/ReportDemoController.cs
@@ -47,16 +47,44 @@ namespace mvcReduxLab.Areas.MVCLab.Controllers
             ds.Account.AddAccountRow("id01", "id01", "id01", "2012/12/12", "12:34", "測試");
             ds.Account.AddAccountRow("id02", "id02", "id02", "2012/12/12", "12:34", "測試");
 
+            // 模擬 EF6 查詢傳值回來
+            var qry = from c in ds.Account.AsEnumerable()
+                      select new AccountInfo {
+                          name = c.name,
+                          email = c.email,
+                          mobilePhone = c.mobilePhone,
+                          birthday = c.birthday,
+                          contactTime = c.contactTime,
+                          remark = c.remark
+                      };
+
             // Set report info
             ReportWrapper rw = new ReportWrapper();
             rw.ReportPath = Server.MapPath("~/Report/rdlc/Report1.rdlc");
-            rw.ReportDataSources.Add(new ReportDataSource("Account", (DataTable)ds.Account));
+
+            // 可以 IEnumerable 填入report data source
+            rw.ReportDataSources.Add(new ReportDataSource("Account", qry));
+
+            //// 或以 DataTable 填入report data source
+            //rw.ReportDataSources.Add(new ReportDataSource("Account", (DataTable)ds.Account));
 
             // Pass report info via session
             Session["ReportWrapper"] = rw;
 
             // Go report viewer page
             return Redirect("~/Report/ReportViewer.aspx");
+        }
+
+        /// <summary>
+        /// helper class
+        /// </summary>
+        private class AccountInfo {
+            public string name { get; set; }
+            public string email { get; set; }
+            public string mobilePhone { get; set; }
+            public string birthday { get; set; }
+            public string contactTime { get; set; }
+            public string remark { get; set; }
         }
     }
 }
