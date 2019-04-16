8f7215ded2b6a2c93512f602de4c9cd06e4219f2
Rely Ky <relyky@hotmail.com>
2019-04-16 17:40:59 +0800

    ReportViewer

--- mvcReduxLab/Areas/MVCLab/Controllers/ReportDemoController.cs
+++ mvcReduxLab/Areas/MVCLab/Controllers/ReportDemoController.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Reporting.WebForms;
+using mvcReduxLab.Report;
+using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -23,5 +26,37 @@ namespace mvcReduxLab.Areas.MVCLab.Controllers
             }
         }
 
+        /// <summary>
+        /// 列印
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Print()
+        {
+            // Go report viewer page
+            return Redirect("~/Report/Report1.aspx");
+        }
+
+        /// <summary>
+        /// 列印(一般化)
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Report()
+        {
+            // 準備資料來源
+            ReportDataSet ds = new ReportDataSet();
+            ds.Account.AddAccountRow("id01", "id01", "id01", "2012/12/12", "12:34", "測試");
+            ds.Account.AddAccountRow("id02", "id02", "id02", "2012/12/12", "12:34", "測試");
+
+            // Set report info
+            ReportWrapper rw = new ReportWrapper();
+            rw.ReportPath = Server.MapPath("~/Report/rdlc/Report1.rdlc");
+            rw.ReportDataSources.Add(new ReportDataSource("Account", (DataTable)ds.Account));
+
+            // Pass report info via session
+            Session["ReportWrapper"] = rw;
+
+            // Go report viewer page
+            return Redirect("~/Report/ReportViewer.aspx");
+        }
     }
 }
---
+++ mvcReduxLab/Report/ReportViewer.aspx.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace mvcReduxLab.Report
+{
+    public partial class ReportViewer : System.Web.UI.Page
+    {
+        /// <summary>
+        ///  透過 Session 交換報表資訊過來並產生報表。
+        /// </summary>
+        private void GenerateReport()
+        {
+            var ReportWrapperSessionKey = "ReportWrapper";
+            var rw = (ReportWrapper)Session[ReportWrapperSessionKey];
+            if (rw != null)
+            {
+                // Rdlc location
+                ReportViewer1.LocalReport.ReportPath = rw.ReportPath;
+
+                // Set report data source
+                ReportViewer1.LocalReport.DataSources.Clear();
+                foreach (var reportDataSource in rw.ReportDataSources)
+                    ReportViewer1.LocalReport.DataSources.Add(reportDataSource);
+
+                // Set report parameters
+                ReportViewer1.LocalReport.SetParameters(rw.ReportParameters);
+
+                // Refresh report
+                ReportViewer1.LocalReport.Refresh();
+
+                // Download report directly as Excel file.
+                if (rw.IsDownloadDirectly)
+                {
+                    Warning[] warnings;
+                    string[] streamids;
+                    string mimeType;
+                    string encoding;
+                    string extension;
+
+                    byte[] bytes = ReportViewer1.LocalReport.Render(
+                       "Excel", null, out mimeType, out encoding, out extension,
+                       out streamids, out warnings);
+
+                    Response.Clear();
+                    Response.AddHeader("Content-Disposition", "attachment; filename=sample.xls");
+                    Response.AddHeader("Content-Length", bytes.Length.ToString());
+                    Response.ContentType = "application/octet-stream";
+                    Response.OutputStream.Write(bytes, 0, bytes.Length);
+                }
+
+                // Remove session
+                Session[ReportWrapperSessionKey] = null;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GenerateReport();
+            }
+        }
+
+    }
+}
